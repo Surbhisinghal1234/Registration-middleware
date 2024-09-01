694f4f9abbc07dac78a13ab6c13c5d7c80e90d81
Surbhi Singhal <surbhi98singhal@gmail.com>
2024-09-01 21:14:18 +0530

    remove changes

--- middleware/ipLogger.js
+++ middleware/ipLogger.js
@@ -1,10 +1,9 @@
 import fs from "fs";
 export function ipLogger(req, res, next) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   //   const ip = req.ip;
   //   const url = req.url;
 
-  const { url, method } = req;
+  const {ip, url, method } = req;
   const dataToWrite = ip + " - " + url + " - " + method;
 
   fs.appendFile("access.log", dataToWrite, (err) => {
